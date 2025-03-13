d329cf154619608e15fa0d72d2eed8c8c4dc54ff
gprohorov <g.prokhorov@indevlab.com>
2025-03-13 12:14:53 +0200

    http secured basic

---
+++ src/main/java/edu/pzks/security25/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package edu.pzks.security25.config;
+
+
+/*
+  @author   george
+  @project   security25
+  @class  SecurityConfig
+  @version  1.0.0 
+  @since 13.03.25 - 11.47
+*/
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(csrf ->csrf.disable())
+                .authorizeHttpRequests( auth -> auth.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+
+}
