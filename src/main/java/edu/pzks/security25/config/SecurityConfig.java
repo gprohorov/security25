5f2d3877a121c94e72f8fdc4848c352ec9909450
gprohorov <g.prokhorov@indevlab.com>
2025-04-03 11:50:27 +0300

    audit

--- src/main/java/edu/pzks/security25/config/SecurityConfig.java
+++ src/main/java/edu/pzks/security25/config/SecurityConfig.java
@@ -10,8 +10,10 @@ package edu.pzks.security25.config;
 */
 
 import org.springframework.aop.Advisor;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Role;
 import org.springframework.security.authorization.method.AuthorizationManagerBeforeMethodInterceptor;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -28,6 +30,8 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Bean
+    @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
     public static Advisor preAuthorizeMethodInterceptor() {
         return AuthorizationManagerBeforeMethodInterceptor.preAuthorize();
     }
