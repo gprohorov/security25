3e4338cde46cd7b95acd6c7d2a1f8d7e2b6b6c6a
gprohorov <g.prokhorov@indevlab.com>
2025-04-10 10:25:31 +0300

    Junit

---
+++ src/test/java/edu/pzks/security25/AccessTests.java
@@ -0,0 +1,61 @@
+package edu.pzks.security25;
+
+
+/*
+  @author   george
+  @project   security25
+  @class  AccessTests
+  @version  1.0.0 
+  @since 10.04.25 - 09.59
+*/
+
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ActiveProfiles("test")
+public class AccessTests {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @BeforeAll
+    void beforeAll() {
+        this.mockMvc = MockMvcBuilders
+                .webAppContextSetup(this.webApplicationContext)
+                .build();
+    }
+
+    @Test
+    @WithAnonymousUser
+    public void whenAnonymThenStatusUnautorized() throws Exception {
+
+         mockMvc.perform(get("/api/v1/items"))
+                 .andExpect(status().isUnauthorized());
+
+    }
+
+
+
+}
